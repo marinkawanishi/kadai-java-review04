79e4ec2b7897ab0be347df9393b233df52a088a8
fujit <fujit@user2>
2023-04-18 23:09:27 +0900

    kadai7

---
+++ src/animal/Human.java
@@ -0,0 +1,23 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+    private String hobby;
+
+    public Human(String name, int age, String hobby) {
+        super(name, age);
+        this.hobby = hobby;
+}
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public void think() {
+        System.out.println("私は" + hobby + "について考えています。");
+    }
+}
